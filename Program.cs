d3b0631fe7d18ee578ade8dd1ff1561e17bf6db2
ErickFPCruz <erick.cruz97@hotmail.com>
2022-03-25 18:06:25 -0300

    Vamos pedir para qualquer um!!!! Urgente!!!!

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+string fraseDigitada, fraseDoCebolinha;
+
+Console.Write($"Digite qualquer coisa: {Console.ReadLine()}");
+
+fraseDigitada = Console.ReadLine();
+
+fraseDoCebolinha = fraseDigitada.Replace("r", "l").Replace("R", "L");
+
+Console.WriteLine(fraseDoCebolinha);
